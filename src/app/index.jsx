1cfa06d775a5f487ea20430ce219d4b9cacd085f
Windham Wong <windhamwong@nva-hk.com>
2017-09-09 15:40:07 +0100

    Mid-Week Working state

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class NotePasta extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+import { Button, Form, FormGroup, FormControl, } from 'react-bootstrap';
+
+import CodeMirror from 'react-codemirror';
+// import 'codemirror/theme/monokai.css';
+import markdown from 'codemirror/mode/markdown/markdown';
+
+class NoteMain extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'value': '',
+            'code': 'aaaa **aaaaaa**',
+        }
+    }
+
+    render () {
+        let editorCSS = {
+            lineNumbers: true,
+            mode: markdown,
+            theme: 'monokai',
+        }
+
+        return (
+<div className="noteMain">
+    <div className="searchCol">
+        <Form><FormGroup controlId="formBasicText">
+            <FormControl bsClass="searchBar" type="text" value={this.state.value} placeholder="Keyword"
+            onChange = {(event) => {this.setState({'value':event.target.value})}} />
+            <FormControl.Feedback />
+        </FormGroup></Form>
+    </div>
+
+        <CodeMirror className={'noteEditor'} value={this.state.code} onChange={(value) => {this.setState({code: value})}} options={editorCSS} />
+</div>
+);
+    }
 }
 
-render(<NotePasta/>, document.getElementById('app'));
+render(<NoteMain />, document.getElementById('NoteMain'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,8 @@ var config = {
     },
     module : {
         loaders : [
-            {
-                test : /\.jsx?/,
-                include : APP_DIR,
-                loader : 'babel-loader'
-            }
+            {test : /\.jsx?/, include : APP_DIR, loader : 'babel-loader'},
+            {test: /\.css$/, loader: "style-loader!css-loader"},
         ]
     }
 };
