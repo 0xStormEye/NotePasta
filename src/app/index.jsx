c7791a1e791b3a8aefe5a95d65e61d5186c85170
Windham Wong <windhamwong@nva-hk.com>
2017-09-26 10:43:41 +0100

    PouchDB implemented Pasta editor now auto retrieve saved data from db.

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,13 +9,40 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/javascript/javascript';
 
+import PouchDB from 'pouchdb-browser';
+let db = new PouchDB('notePasta');
+
 class LeftColumn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
-            code: 'aaaa **aaaaaa**',
+            code: '',
         }
+
+        this.retrievePasta();
+    }
+
+    async retrievePasta() {
+        let result = await db.get('notePasta');
+        if (result) {
+            this.setState({code: result.content});
+        }
+    }
+
+    savePasta() {
+        let data = {
+            _id: "notePasta",
+            date: new Date().toISOString(),
+            content: this.state.code,
+        }
+        db.put(data, function callback(err, result) {
+            if (!err) {
+                console.log("Successfully preserved the pasta in the fridge.");
+            } else {
+                console.log("Something wrong with the fridge. " + err);
+            }
+        });
     }
 
     render () {
@@ -33,6 +60,7 @@ class LeftColumn extends React.Component {
         onChange = {(event) => {this.setState({'value':event.target.value})}} />
     <ButtonToolbar className="searchBarButton">
             <Button bsSize="small">Default</Button>
+            <Button bsSize="small" onClick={() => {this.savePasta();}}>Save Note</Button>
         </ButtonToolbar>
     </div>
 
@@ -66,14 +94,13 @@ class RightColumn extends React.Component {
     }
 
     render () {
-        let PastaReadyEditorCSS = {
+        let PastaPrepareEditorCSS = {
             mode: 'javascript',
             theme: 'monokai',
             lineWrapping: true,
         }
 
-        let PastaDishEditorCSS = {
-            lineNumbers: true,
+        let PastaFinalEditorCSS = {
             mode: 'javascript',
             theme: 'monokai',
             lineWrapping: true,
@@ -81,15 +108,15 @@ class RightColumn extends React.Component {
         return (
 <div className="RightMain container-fluid nopadding">
         <div className="InputSection col nopadding">
-            <FormControl bsClass="searchBar" type="text" value={this.state.src} placeholder="Keyword"
+            <FormControl bsClass="searchBar" type="text" value={this.state.src} placeholder="Source IP Address"
             onChange = {(event) => {this.setState({'src':event.target.value})}} />
-        <FormControl bsClass="searchBar" type="text" value={this.state.dest} placeholder="Keyword"
+        <FormControl bsClass="searchBar" type="text" value={this.state.dest} placeholder="Destination IP Address"
             onChange = {(event) => {this.setState({'dest':event.target.value})}} />
-        <CodeMirror className={'pastaEditor'} value={this.state.prepareNote} onChange={(value) => {this.setState({prepareNote: value}); this.finalNoteRender();}} options={PastaReadyEditorCSS} />
+        <CodeMirror className={'pastaEditor'} value={this.state.prepareNote} onChange={(value) => {this.setState({prepareNote: value}); this.finalNoteRender();}} options={PastaPrepareEditorCSS} />
         </div>
 
         <div className="OutputSection col nopadding">
-            <CodeMirror className={'pastaEditor'} value={this.state.finalNote} options={PastaDishEditorCSS} />
+            <CodeMirror className={'pastaEditor'} value={this.state.finalNote} options={PastaFinalEditorCSS} />
         </div>
 </div>
 );
