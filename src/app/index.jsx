41944114bda2c12babfe78afb2c4bc5f48c4e479
Windham Wong <windhamwong@nva-hk.com>
2017-10-21 15:46:03 +0100

    latest work sync up

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,6 +9,8 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/javascript/javascript';
 
+import Modal from 'reboron/DropModal';
+
 import PouchDB from 'pouchdb-browser';
 let db = new PouchDB('notePasta');
 
@@ -18,31 +20,84 @@ class LeftColumn extends React.Component {
         this.state = {
             value: '',
             code: '',
+            pastaNotes: [],
+            rev: '',
+            newPastaTitle: '',
+            newPastaContent: '',
+            newPastaKeyword: '',
         }
 
         this.retrievePasta();
     }
 
+    async deletePasta(_id, _rev) {
+        // let result = await db.allDocs().then((doc)=>{console.log(doc);for (var item in doc.rows) {console.log(item.id);db.remove(item.id, item._rev);}});
+        // console.log(result);
+        // if (result) {
+        //     this.setState({code: result.content, rev: result._rev, pastaNotes: result.pastaNotes});
+        // }
+        db.remove(_id, _rev);
+
+    }
+
     async retrievePasta() {
         let result = await db.get('notePasta');
         if (result) {
-            this.setState({code: result.content});
+            this.setState({code: result.content, rev: result._rev, pastaNotes: result.pastaNotes});
         }
     }
 
-    savePasta() {
+    async newPasta() {
+        console.log(this.state);
         let data = {
-            _id: "notePasta",
+            _id: this.state.newPastaTitle,
+            content: this.state.newPastaContent,
+            keyword: this.state.newPastaKeyword,
             date: new Date().toISOString(),
-            content: this.state.code,
         }
-        db.put(data, function callback(err, result) {
+        db.put(data, {force: true}, function callback(err, result) {
             if (!err) {
                 console.log("Successfully preserved the pasta in the fridge.");
             } else {
                 console.log("Something wrong with the fridge. " + err);
             }
         });
+    }
+
+    async updatePasta() {
+        console.log(this.state);
+        let data = {
+            _id: this.state.updatePastaTitle,
+            content: this.state.updatePastaContent,
+            keyword: this.state.updatePastaKeyword,
+            date: new Date().toISOString(),
+        }
+        db.put(data, {force: true}, function callback(err, result) {
+            if (!err) {
+                console.log("Successfully preserved the pasta in the fridge.");
+            } else {
+                console.log("Something wrong with the fridge. " + err);
+            }
+        });
+    }
+
+    newPastaPopup() {
+        let newPastaContentEditorCSS = {
+            lineNumbers: true,
+            mode: 'javascript',
+            theme: 'monokai',
+            lineWrapping: true,
+        }
+        return (
+<div>
+    <Modal ref="modal" keyboard={this.callback} className="newPastaPopup">
+        <FormControl bsClass="searchBar" type="text" value={this.state.newPastaTitle} placeholder="Title"
+        onChange = {(event) => {this.setState({'newPastaTitle':event.target.value})}} />
+    <CodeMirror className={'newPastaEditor'} value={this.state.newPastaContent} onChange={(value) => {this.setState({newPastaContent: value})}} options={newPastaContentEditorCSS} />
+        <button onClick={() => {this.newPasta(); this.hideModal;}}>Save</button>
+    </Modal>
+</div>
+        );
     }
 
     render () {
@@ -55,12 +110,13 @@ class LeftColumn extends React.Component {
 
         return (
 <div className="LeftMain">
+    {this.newPastaPopup()}
     <div className="searchCol">
         <FormControl bsClass="searchBar" type="text" value={this.state.value} placeholder="Keyword"
         onChange = {(event) => {this.setState({'value':event.target.value})}} />
     <ButtonToolbar className="searchBarButton">
-            <Button bsSize="small">Default</Button>
-            <Button bsSize="small" onClick={() => {this.savePasta();}}>Save Note</Button>
+            <Button bsSize="small" onClick={() => {this.refs.modal.show();}}>New Pasta</Button>
+            <Button bsSize="small" onClick={() => {}}>Save Pasta</Button>
         </ButtonToolbar>
     </div>
 
